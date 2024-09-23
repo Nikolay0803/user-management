937217a0b98c49f74d2b76b8a303299105925169
Mykola_Maslakai <96542705+Nikolay0803@users.noreply.github.com>
2024-09-23 22:04:55 +0300

    updete versel

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "http://13.60.194.15:80/:path*",
+      },
+      { source: "/(.*)", destination: "/" },
+    ];
+  },
+};
 
 export default nextConfig;
